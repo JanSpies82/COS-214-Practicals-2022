ecf8c2f616e110e410fa9c79244fdab288890de9
DeadpanSplash82 <janco.spies@gmail.com>
2022-08-22 15:57:38 +0200

    Added Confectionary functions

--- Prac 3/Confectionary.cpp
+++ Prac 3/Confectionary.cpp
@@ -1,12 +1,31 @@
 #include "Confectionary.h"
 using namespace std;
-int Confectionary::counter = 0;
+int Confectionary::counter = 1;
 
-Confectionary::Confectionary(string manufacturer, double price, string type)
+Confectionary::Confectionary(string manufacturer, double price, string type) : manufacturer(manufacturer), price(price), type(type)
 {
-}
+	id = counter++;
+};
 
 string Confectionary::getDescription()
 {
-	return "";
+	string out = "";
+	out += "Confectionary " + to_string(id) + ": " + manufacturer + " " + type + " costs " + to_string(price);
+	return out;
 }
+double Confectionary::getPrice()
+{
+	return price;
+};
+std::string Confectionary::getManufacturer()
+{
+	return manufacturer;
+};
+std::string Confectionary::getType()
+{
+	return type;
+};
+int Confectionary::getId()
+{
+	return id;
+};
