bbe4b88762ddacef48475bc29286b4774928be42
DeadpanSplash82 <janco.spies@gmail.com>
2022-09-02 09:14:09 +0200

    Completed #1

--- Prac 4/marks.cpp
+++ Prac 4/marks.cpp
@@ -1,18 +1,20 @@
- #include <iostream>
+#include <iostream>
 
- using namespace std;
+using namespace std;
 
- int improve(int, int);
+int improve(int, int);
 
- int main() {
-    int mark = 59, highest = 87;
-    cout << improve(mark, highest);
-    mark = -2; highest = 0;
-    cout << improve(mark, highest);
-    return 0;
- }
-
- int improve(int a, int b)
+int main()
 {
-    return ((double)(a / b)) * 100;
- } 
+   int mark = 59, highest = 87;
+   cout << improve(mark, highest);
+   mark = -2;
+   highest = 0;
+   cout << improve(mark, highest);
+   return 0;
+}
+
+int improve(int a, int b)
+{
+   return ((double)(a / b)) * 100;
+}
